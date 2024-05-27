0db8444d2abfaf470b75854a24a8cd64154368b7
aljo <johansson_a95@hotmail.com>
2024-05-27 23:22:38 +0200

    finish: mobile version & animations

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -1,29 +1,98 @@
+
 <template>
-    <div id="all-nav-container">
+    <div id="all-nav-container" v-if="!isMobile">
         <div id="nav-container">
             <router-link to="/" exact tag="div" class="rl">Home</router-link>
             <router-link to="/about" exact tag="div" class="rl">About me</router-link>
-            <router-link to="/about" class="rl">Portfolio</router-link>
-            <router-link to="/about" class="rl">Skills</router-link>
-            <router-link to="/about" class="rl">Resume</router-link>
-            <router-link to="/about" class="rl">Contact</router-link>
+            <router-link to="/portfolio" class="rl">Portfolio</router-link>
+            <router-link to="/skills" class="rl">Skills</router-link>
+            <router-link to="/resume" class="rl">Resume</router-link>
+            <router-link to="/contact" class="rl">Contact</router-link>
         </div>
         <div id="img-links">
-            <p id="GH">GH</p>
-            <p id="LI">LI</p>
-            <p id="ML">MAIL</p>
+            <a href="https://github.com/aljo95">
+                <img id="GH-Icon" src="/github-mark-white.png"></img>
+            </a>
+            <a href="https://www.linkedin.com/in/johansson-alex/">
+                <img id="LI-Icon" src="/linkedin-icon.png"></img>
+            </a>
+            <a href="mailto:alejoh.github.95@gmail.com">
+                <img id="MAIL-Icon" src="/email-icon.png"></img>
+            </a>
         </div>
     </div>
+    <!--<div id="mobile-nav" v-if="isMobile"> v-if="!isMobileMenuClicked && isMobile"></img> -->
+        <img id="mobile-menu" src="/menuIconMobile.png" @click="toggleMenu" v-if="isMobile"></img>
+        <div id="mobile-nav-container" v-if="isMobileMenuClicked && isMobile">
+            <!--<p id="exit-button" @click="toggleMenu">X</p>-->
+            <img id="mobile-menu-opened" src="/menuIconMobile.png" @click="toggleMenu" ></img>
+            <div id="mobile-routes-container">
+                <router-link to="/" exact tag="div" class="rl mrl">Home</router-link>
+                <router-link to="/about" exact tag="div" class="rl mrl">About me</router-link>
+                <router-link to="/portfolio" class="rl mrl">Portfolio</router-link>
+                <router-link to="/skills" class="rl mrl">Skills</router-link>
+                <router-link to="/resume" class="rl mrl">Resume</router-link>
+                <router-link to="/contact" class="rl mrl">Contact</router-link>
+            </div>
+            <div id="mobile-icon-links">
+                <a href="https://github.com/aljo95">
+                    <img id="GH-Icon-Mobile" src="/github-mark-white.png"></img>
+                </a>
+                <a href="https://www.linkedin.com/in/johansson-alex/">
+                    <img id="LI-Icon-Mobile" src="/linkedin-icon.png"></img>
+                </a>
+                <a href="mailto:alejoh.github.95@gmail.com">
+                    <img id="MAIL-Icon-Mobile" src="/email-icon.png"></img>
+                </a>
+            </div>
+        </div>
+    <!--</div> -->
 </template>
-
-
 
 
 <script>
 export default {
+    data() {
+        return {
+            isMobile: false,
+            isMobileMenuClicked: false
+        }
+    },
+    created: function() {
+        window.addEventListener("resize", (e) => {
+            //console.log(e.target.innerWidth)
+            if (e.target.innerWidth <= 1180) this.isMobile=true
+            else if (e.target.innerWidth > 1180) this.isMobile=false
+        });
+    },
+    destroyed: function() {
+        window.removeEventListener("resize", e);
+    },
+    mounted() {
+        //console.log(window.innerWidth)
+        if (window.innerWidth <= 1180) this.isMobile=true
+        else if (window.innerWidth > 1180) this.isMobile=false
+    },
+    methods: {
+        toggleMenu() {
+            if (document.querySelector("#mobile-nav-container")) {
+                document.querySelector("#mobile-nav-container").classList.toggle("classSlideIn")
+                setTimeout(() => {
+                    this.isMobileMenuClicked = !this.isMobileMenuClicked;
+                    return;
+                }, 200)
+            } else {
+                this.isMobileMenuClicked = !this.isMobileMenuClicked;
+                setTimeout(() => {
+                    if (this.isMobileMenuClicked)
+                        document.querySelector("#mobile-nav-container").classList.toggle("classSlideOut");
+                    
+                }, 10)
+            }
+        }
+    }
 }
 </script>
-
 
 
 <style scoped>
@@ -56,8 +125,11 @@ export default {
     max-width: 2000px;
     animation: slidin 2.5s;
     
+	will-change: transform;
+    
+    
 }
-#nav-container {                 /* */
+#nav-container {
     width: 100%;
     height: 60%;
     display: flex;
@@ -90,7 +162,7 @@ export default {
 .router-link-exact-active {
     color: #7e9af5;
 }
-#img-links {            /* */
+#img-links {
     height: 25%;
     display: flex;
     align-items: center;
@@ -117,11 +189,10 @@ p {
         border-radius: 5%;
     }
     60% {
-        height: 29.9%;
-        
+        height: 275px;
     }
     100% {
-        height: 29.9%;
+       height: 275px;
     }
 }
 @keyframes fadeInAfterFinish {
@@ -135,5 +206,109 @@ p {
         opacity: 1;
     }
 }
-</style>
+#GH-Icon {
+    width: 22.5px;
+    height: 22.5px;
+    padding-top: 30%;
+}
+#LI-Icon {
+    width: 17.5px;
+    height: 17.5px;
+    padding-top: 30%;
+}
+#MAIL-Icon {
+    width: 30px;
+    height: 30px;
+    padding-top: 25%;
+}
 
+/*****  Mobile *****/
+#mobile-menu { /* Icon */
+    width: 33px;
+    height: 33px;
+    background-color: #13141a;
+    padding: 5%;
+    border-radius: 0 0 15px 0;
+    position: absolute;
+    
+}
+#mobile-nav-container {
+    position: absolute;
+    width: 240px;
+    height: 340px;
+    background-color: #13141a;
+    z-index: 1;
+    left: -60%;
+}
+.classSlideOut {
+    animation: slideOutMobileMenu 0.2s linear forwards;
+}
+.classSlideIn {
+    animation: slideInMobileMenu 0.2s linear forwards;
+}
+@keyframes slideOutMobileMenu {
+    0% {
+        left: -60%;
+    }
+    100% {
+        left: 0%;
+    }
+}
+@keyframes slideInMobileMenu {
+    0% {
+        left: 0%;
+    }
+    100% {
+        left: -60%;
+    }
+}
+#mobile-menu-opened {
+    float:right;
+    font-size: 24px;
+    width: 33px;
+    height: 33px;
+    padding: 5%;
+    margin-top: 7px;
+    margin-left: 7px;
+    z-index: 2;
+}
+#mobile-menu-opened:active { 
+    background-color: #49494e;
+}
+#mobile-routes-container {
+    height: 70%;
+    width: 60%;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: space-evenly;
+    padding-top: 5%;
+    padding-bottom: 5%;
+    padding-left: 5%;
+}
+.mrl {
+    font-size: 22px;
+    font-weight: bold;
+}
+#mobile-icon-links {
+    width: 100%;
+    height: 15%;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    padding-top: 10px;
+}
+#GH-Icon-Mobile {
+    width: 35px;
+    height: 35px;
+}
+#LI-Icon-Mobile {
+    width: 30px;
+    height: 30px;
+}
+#MAIL-Icon-Mobile {
+    width: 40px;
+    height: 40px;
+    padding-top: 7.5px;
+}
+</style>
