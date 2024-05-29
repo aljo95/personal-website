e16c6d8236f90b91a9b1eec91d404bdc16a5c7db
aljo <johansson_a95@hotmail.com>
2024-05-29 22:52:16 +0200

    initial main app file

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+
+<template>
+  <nav-menu></nav-menu>
+  <div id="wrapper">
+    <router-view></router-view>
+  </div>
+</template>
+
+
+<script>
+export default {
+}
+</script>
+
+<style>
+html, body, #app {
+  background-color: #090d14;
+  width: 100%;
+  height: 100%;
+  margin: 0;
+}
+
+#wrapper {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
