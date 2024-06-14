06f63390d93de4381a5faffdae719365771a0fbb
aljo <johansson_a95@hotmail.com>
2024-06-14 20:39:02 +0200

    preloading necessary assets

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@
 
 <script>
 export default {
+
 }
 </script>
 
@@ -26,5 +27,30 @@ html, body, #app {
   display: flex;
   justify-content: center;
   align-items: center;
+}
+html {
+  background-image: url('/skills-icons/angular1.png'),
+    url('/skills-icons/asm1.png'),
+    url('/skills-icons/c1.png'),
+    url('/skills-icons/djanog1.png'),
+    url('/skills-icons/elixir1.png'),
+    url('/skills-icons/express1.png'),
+    url('/skills-icons/java1.png'),
+    url('/skills-icons/js1.png'),
+    url('/skills-icons/mDB1.png'),
+    url('/skills-icons/mySQL1.png'),
+    url('/skills-icons/next1.png'),
+    url('/skills-icons/postgreSQL1.png'),
+    url('/skills-icons/py1.png'),
+    url('/skills-icons/react1.png'),
+    url('/skills-icons/spring1.png'),
+    url('/skills-icons/tailwind1.png'),
+    url('/skills-icons/ts1.png'),
+    url('/skills-icons/vue1.png'),
+    url('/contact/background-logo.png');
+    
+
+
+    background-size: 0;
 }
 </style>
