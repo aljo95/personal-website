b1c2f05004c83d56c3c515a61f6adb42d8de8313
aljo <johansson_a95@hotmail.com>
2024-06-14 22:09:04 +0200

    new preload func

--- src/App.vue
+++ src/App.vue
@@ -32,7 +32,7 @@ html {
   background-image: url('/skills-icons/angular1.png'),
     url('/skills-icons/asm1.png'),
     url('/skills-icons/c1.png'),
-    url('/skills-icons/djanog1.png'),
+    url('/skills-icons/django1.png'),
     url('/skills-icons/elixir1.png'),
     url('/skills-icons/express1.png'),
     url('/skills-icons/java1.png'),
@@ -47,9 +47,36 @@ html {
     url('/skills-icons/tailwind1.png'),
     url('/skills-icons/ts1.png'),
     url('/skills-icons/vue1.png'),
-    url('/contact/background-logo.png');
-    
-
+    url('/contact/background-logo.png'),
+    url('/contact/email-icon.png'),
+    url('/contact/github-mark-white.png'),
+    url('/contact/linkedin-icon.png'),
+    url('/contact/phone-icon-white.png'),
+    url('/cats/g/g1.jpg'),
+    url('/cats/g/g2.jpg'),
+    url('/cats/k/k1-fix.jpg'),
+    url('/cats/k/k2.jpg'),
+    url('/cats/k/k3.jpg'),
+    url('/cats/k/k4.jpg'),
+    url('/cats/m/m1.jpg'),
+    url('/cats/m/m2.jpg'),
+    url('/cats/m/m3.jpg'),
+    url('/cats/m/m4.jpg'),
+    url('/cats/m/m5.jpg'),
+    url('/cats/z/z1.jpg'),
+    url('/cats/z/z2.jpg'),
+    url('/cats/z/z3.jpg'),
+    url('/portfolio-imgs/epr-bigimg.png'),
+    url('/portfolio-imgs/gh-icon-port.png'),
+    url('/portfolio-imgs/link-icon-port.png'),
+    url('/portfolio-imgs/nc-bigimg.png'),
+    url('/portfolio-imgs/y1.png'),
+    url('/portfolio-imgs/y2.png'),
+    url('/resume/cv-image.png'),
+    url('/nav-icons/email-icon.png'),
+    url('/nav-icons/github-mark-white.png'),
+    url('/nav-icons/linkedin-icon.png'),
+    url('/nav-icons/menuIconMobile.png');
 
     background-size: 0;
 }
